3400abb4bc28ed9f962bdff311003a90f2596191
wuziyan <wuziyan@hyperchain.cn>
2020-08-17 15:52:52 +0800

    state hook

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  // 函数调用将 state 添加到组件，并将其值用 0 进行初始化
+  // 该函数返回一个包含两个元素的数组
+  // 通过解构赋值语法将元素分配给变量 counter 和 setCounter 
+  const [ counter, setCounter ] = useState(0)
+  //counter 变量被赋予的初始值state 为零
+  // 变量 setCounter 被分配给一个函数，该函数将用于修改 state
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
 
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
