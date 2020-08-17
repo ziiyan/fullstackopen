7f8bd0a97cabcc6e87f5ee77a15b506fc5ff8514
wuziyan <wuziyan@hyperchain.cn>
2020-08-17 19:56:57 +0800

    TODO: deal with points state

---
+++ part1-anecdotes/src/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Anecdote = ({text, val}) => {
+  return (
+    <>
+      <div>{text}</div>
+      <div>has {val} votes</div>
+    </>
+  )
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const copy = [...points]
+
+  const handleVote = () => {
+    console.log(props.points, copy)
+    copy[selected] += 1
+  }
+  const handleNext = () => {
+    setSelected((selected + 1) % len)
+  }
+
+  return (
+    <div>
+      <Anecdote text={props.anecdotes[selected]} val={copy[selected]} />
+      <Button onClick={handleVote} text='vote' />
+      <Button onClick={handleNext} text='next anecdote' />
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+const len = anecdotes.length
+const points = new Array(len).fill(0)
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} points={points} />,
+  document.getElementById('root')
+)
